38179ffb9f31f621d35298e60cdd942ef2d4d357
Leonel Abud <abudleito@gmail.com>
2025-03-17 12:14:09 -0300

    Primeros Ejercicios

---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+
+    int edad;
+    float pesoEnKg;
+    string nombre;
+    bool validoParaElTrabajo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        edad = 21;
+        pesoEnKg = 81.4f;
+        nombre = "Gabriel";
+        validoParaElTrabajo = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
